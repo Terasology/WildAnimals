17f7251d2733174586824989ad49ade44fe5b733
Rasmus Praestholm <cervator@gmail.com>
2016-02-27 20:28:50 -0500

    Move Assets class from org.terasology.asset to org.terasology.utilities.

--- src/main/java/org/terasology/wildAnimals/WildAnimalsSpawnSystem.java
+++ src/main/java/org/terasology/wildAnimals/WildAnimalsSpawnSystem.java
@@ -16,7 +16,7 @@
 package org.terasology.wildAnimals;
 
 import com.google.common.collect.Lists;
-import org.terasology.asset.Assets;
+import org.terasology.utilities.Assets;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
