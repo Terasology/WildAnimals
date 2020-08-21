7ec8046e68b7792743ffc46e0887995703646f1f
DarkWeird <foxflameless@gmail.com>
2020-08-21 17:53:09 +0300

    fix: NUI extraction compability

--- src/main/java/org/terasology/wildAnimals/WorldlyTooltipDisplay/WildAnimalsTooltipSystem.java
+++ src/main/java/org/terasology/wildAnimals/WorldlyTooltipDisplay/WildAnimalsTooltipSystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.wildAnimals.WorldlyTooltipDisplay;
 
 import org.terasology.entitySystem.entity.EntityRef;
@@ -22,7 +9,7 @@ import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.common.DisplayNameComponent;
 import org.terasology.rendering.nui.layers.ingame.inventory.GetItemTooltip;
-import org.terasology.rendering.nui.widgets.TooltipLine;
+import org.terasology.nui.widgets.TooltipLine;
 import org.terasology.wildAnimals.component.WildAnimalComponent;
 import org.terasology.worldlyTooltipAPI.events.GetTooltipIconEvent;
 import org.terasology.worldlyTooltipAPI.events.GetTooltipNameEvent;
