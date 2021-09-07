e398be3b618e1e2829cc0e82e16be4ab8c25a10c
darkweird <foxflameless@gmail.com>
2021-09-07 17:55:26 +0300

    feature(gestaltv7-events): replace TS's Event to gestaltv7's Event

--- src/main/java/org/terasology/wildAnimals/event/AnimalGrowthEvent.java
+++ src/main/java/org/terasology/wildAnimals/event/AnimalGrowthEvent.java
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.wildAnimals.event;
 
-import org.terasology.engine.entitySystem.event.Event;
+import org.terasology.gestalt.entitysystem.event.Event;
 
 public class AnimalGrowthEvent implements Event {
 }
