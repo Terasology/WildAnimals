158a008ec595daacf61fe8696bae66031859743a
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-06-16 14:18:45 -0700

    test: switch how we load the saved game

--- src/test/java/org/terasology/wildAnimals/system/WildAnimalsSpawnSystemTest.java
+++ src/test/java/org/terasology/wildAnimals/system/WildAnimalsSpawnSystemTest.java
@@ -7,7 +7,7 @@ import com.google.common.collect.Lists;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
@@ -18,18 +18,14 @@ import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.engine.config.Config;
 import org.terasology.engine.config.SystemConfig;
 import org.terasology.engine.context.Context;
 import org.terasology.engine.core.GameEngine;
 import org.terasology.engine.core.PathManager;
 import org.terasology.engine.core.Time;
-import org.terasology.engine.core.bootstrap.EntitySystemSetupUtil;
-import org.terasology.engine.core.module.ModuleManager;
 import org.terasology.engine.core.subsystem.EngineSubsystem;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.entity.internal.EngineEntityManager;
 import org.terasology.engine.entitySystem.prefab.PrefabManager;
 import org.terasology.engine.integrationenvironment.MainLoop;
 import org.terasology.engine.integrationenvironment.jupiter.Dependencies;
@@ -38,17 +34,17 @@ import org.terasology.engine.integrationenvironment.jupiter.MTEExtension;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.logic.players.LocalPlayer;
 import org.terasology.engine.persistence.StorageManager;
-import org.terasology.engine.persistence.internal.ReadWriteStorageManager;
 import org.terasology.engine.rendering.logic.SkeletalMeshComponent;
+import org.terasology.engine.rendering.nui.layers.mainMenu.savedGames.GameProvider;
 import org.terasology.engine.world.WorldProvider;
-import org.terasology.engine.world.block.BlockManager;
 import org.terasology.engine.world.block.BlockRegion;
+import org.terasology.engine.world.block.BlockRegionc;
 import org.terasology.engine.world.chunks.Chunks;
-import org.terasology.engine.world.chunks.blockdata.ExtraBlockDataManager;
 import org.terasology.wildAnimals.AnimalSpawnConfig;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
@@ -59,7 +55,7 @@ import java.util.stream.Collectors;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Tag("MteTest")
 @ExtendWith(MTEExtension.class)
@@ -70,9 +66,11 @@ class WildAnimalsSpawnSystemTest {
 
     private static final Logger logger = LoggerFactory.getLogger(WildAnimalsSpawnSystemTest.class);
 
-    Set<Vector3ic> inhabitedChunks = new HashSet<>();
+    @SuppressWarnings("checkstyle:ConstantName")
+    private static final Set<Vector3ic> inhabitedChunks = new HashSet<>();
+    private static BlockRegionc relevantRegion;
 
-    @BeforeAll
+    @BeforeEach
     void configSpawn(WildAnimalsSpawnSystem spawnSystem, WorldProvider worldProvider) {
         var config = new AnimalSpawnConfig();
         config.spawnChanceInPercent = 100;
@@ -92,10 +90,13 @@ class WildAnimalsSpawnSystemTest {
     @Order(1)
     void testCanSave(EntityManager entities, StorageManager storage, LocalPlayer player, MainLoop main,
                      WildAnimalsSpawnSystem spawnSystem, PrefabManager prefabs) {
+        assertThat(GameProvider.isSavesFolderEmpty()).isTrue();
+
         var character = player.getCharacterEntity();
         var loc = character.getComponent(LocationComponent.class).getWorldPosition(new Vector3f());
-        main.runUntil(main.makeBlocksRelevant(new BlockRegion((int) loc.x, (int) loc.y, (int) loc.z)
-                .expand(100, 0, 100)));
+        relevantRegion = new BlockRegion((int) loc.x, (int) loc.y, (int) loc.z)
+                .expand(100, 0, 100);
+        main.runUntil(main.makeBlocksRelevant(relevantRegion));
 
 //        var sheepPrefab = prefabs.getPrefab("WildAnimals:sheep");
 //        spawnSystem.tryFlockAnimalSpawn(sheepPrefab, Chunks.toChunkPos(loc, new Vector3i()));
@@ -109,6 +110,7 @@ class WildAnimalsSpawnSystemTest {
                 .map(v -> (Vector3ic) Chunks.toChunkPos(v, new Vector3i()))
                 .collect(Collectors.toUnmodifiableSet())
                 .block();
+        inhabitedChunks.clear();
         inhabitedChunks.addAll(animalPosition);
 
         storage.waitForCompletionOfPreviousSaveAndStartSaving();
@@ -117,30 +119,21 @@ class WildAnimalsSpawnSystemTest {
 
     @Test
     @Order(2)
-    void testCanLoad(ModuleManager modules,
-                     BlockManager blockManager, ExtraBlockDataManager extraDataManager,
-                     Context context, Config config, MainLoop main, Time time) throws IOException {
-        // MTE does not include helpers for loading from a save file.
-        // This is kludged together from StorageManagerTest internals.
-        EntitySystemSetupUtil.addReflectionBasedLibraries(context);
-        EntitySystemSetupUtil.addEntityManagementRelatedClasses(context);
-        EngineEntityManager newEntityManager = context.getValue(EngineEntityManager.class);
-        StorageManager newSM = new ReadWriteStorageManager(getSavePath(config), modules.getEnvironment(), newEntityManager, blockManager,
-                extraDataManager, null, null, null);
-        context.put(StorageManager.class, newSM);
-
-        newSM.loadGlobalStore();
+    void testCanLoad(StorageManager storage, EntityManager entities, MainLoop main, Time time) {
+        assertThat(GameProvider.getSavedGames()).hasSize(1);
+        main.runUntil(main.makeBlocksRelevant(relevantRegion));
 
         for (Vector3ic chunk : inhabitedChunks) {
-            var restored = newSM.loadChunkStore(chunk);
-            /* var dudes = */ restored.restoreEntities();
-            /* logger.warn("restored dudes in {} '{}'", chunk, dudes); */
+            var restored = storage.loadChunkStore(chunk);
+            restored.restoreEntities();
+            // var dudes = restored.restoreEntities();
+            // logger.warn("restored dudes in {} '{}'", chunk, dudes);
         }
 
-//        var later = time.getGameTime() + 2;
-//        main.runUntil(() -> time.getGameTime() > later);
+        var later = time.getGameTime() + 2;
+        main.runUntil(() -> time.getGameTime() > later);
 
-        var animals = Lists.newArrayList(newEntityManager.getEntitiesWith(SkeletalMeshComponent.class));
+        var animals = Lists.newArrayList(entities.getEntitiesWith(SkeletalMeshComponent.class));
         assertThat(animals).isNotNull();
         assertThat(animals).isNotEmpty();
         for (EntityRef animal : animals) {
@@ -152,23 +145,33 @@ class WildAnimalsSpawnSystemTest {
         }
     }
 
-    // copied in from StorageManagerTest
-    Path getSavePath(Config config) {
-        // TODO: add a more direct way of inspecting StorageManager's path.
-        //   This way of getting the game title (and thus the path) is a bit brittle,
-        //   because world title and game title are not _required_ to be identical.
-        String gameTitle = config.getWorldGeneration().getWorldTitle();
-        return PathManager.getInstance().getSavePath(gameTitle);
-    }
-
     static class WriteSaveGames implements EngineSubsystem {
+
+        private static Path homePath;
+
         @Override
         public String getName() {
             return getClass().getCanonicalName();
         }
 
         @Override
+        public void preInitialise(Context rootContext) {
+            if (homePath != null) {
+                try {
+                    logger.debug("Resetting home path to previously seen {}", homePath);
+                    PathManager.getInstance().useOverrideHomePath(homePath);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+        }
+
+        @Override
         public void initialise(GameEngine engine, Context rootContext) {
+            if (homePath == null) {
+                homePath = PathManager.getInstance().getHomePath();
+                logger.debug("Home path first set to {}", homePath);
+            }
             rootContext.getValue(SystemConfig.class).writeSaveGamesEnabled.set(true);
         }
     }
